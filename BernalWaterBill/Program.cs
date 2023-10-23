d507badd595a2045fe902eb0b06a902943fce27b
huawei <huawei@BERNALBENIGNO>
2023-10-23 11:55:50 -0700

    Add project files.

---
+++ BernalWaterBill/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace BernalCashier
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            // Input
+            Console.Write("Enter the previous meter reading: ");
+            int previousReading = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the current meter reading: ");
+            int currentReading = int.Parse(Console.ReadLine());
+
+            //Processing
+            int gallonsUsed = currentReading - previousReading;
+            double totalCharge;
+
+            if ((gallonsUsed / 1000.0) * 7 < 16.67)
+            {
+                totalCharge = 16.67;
+            }
+            else
+            {
+                totalCharge = (gallonsUsed / 1000.0) * 7;
+            }
+
+            // Output
+            Console.WriteLine("Number of gallons used: " + gallonsUsed);
+            Console.WriteLine("Total charge for water: " + totalCharge.ToString("F2") + "pesos");
+        }
+    }
+}
